5cea8e874a2d8bf32cd2e123e2fd2de03fef6053
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-27 23:27:34 +0800

    Updated select procedure.

--- select.js
+++ select.js
@@ -1,146 +1,118 @@
-/*
-	@title: Select Function
-	@author: Richeve S. Bebedor
+/*:
+	@include:
+		{
+			"generate-random-mean-time": "generateRandomMeanTime"
+		}
+	@end-include
 */
-function select( data, cases, scenarios, callback ){
+select = function select( value, cases, callback ){
+	/*
+		A case consist of a condition of acceptance
+			or redirection and an affecting scenario.
 
-	return ( function( config ){
+		By default of a switch case, a switch case
+			contains a value and several cases (condition)
+			if a case fails you can redirect it to the next case
+			but if the case matches, it will execute
+			procedures (scenario)
 
-		//Initialize configuration variable.
-		config = config || { };
+		Selective scenario algorithm takes it to the next level.
+		[
+			{
+				"namespace": "this is optional",
+				"condition": function condition( scope, redirect ){
+					The condition will check the scope's value property
+						and either returns true or redirect
+						the flow to a case.
 
-		//Limit the flow. This will only happen on cyclic non probablistic flows.
-		config.counter = config.counter || 0;
-		config.limit = config.limit || 10; //I don't know about the limit.
-		if( config.counter > config.limit ){
-			throw new Error( "selection procedure overflows" );
-		}
+					If the condition is accepted, it will
+						execute the corresponding scenario.
+					
+					If the condition fails, it will either
+						redirects the flow to other
+						cases or end the flow.
 
-		//Initialize the data to be used and make sure to sync it with the config.
-		data = config.data || ( config.data = data );
-		cases = config.cases || ( config.cases = cases );
-		scenarios = config.scenarios || ( config.scenarios = scenarios );
-		callback = config.callback || ( config.callback = callback );
+					redirect( error | null, true ) 
+						Executes the scenario.
+					redirect( error | null, namespace | [namespace] | null ) 
+						Executes the next case or the chosen cases.
+					redirect( error )
+						Stops the flow because an error occurs.
+					redirect( error | null, false )
+						End the flow.
 
-		//This is for compatibility.
-		var every = config.every 
-			|| ( config.every = 
-				function every( iterator ){
-					var array = this;
-					if( !( this instanceof Array ) ){
-						throw new Error( "invalid array value" );
-					}
-					var index = array.length;
-					while( index-- ){
-						iterator( array[ index ] );
-					}
-				} );
+					Note that if a case requires to execute
+						three redirecting cases then
+						it should complete the flow for the 
+						three cases unless redirect to end.
 
-		//cases is an object consisting of numerical/string 
-		//	comparable data.
-		//scenarios changes the flow of the case
-		var caseCallback = config.caseCallback 
-			|| ( config.caseCallback = 
-				function caseCallback( error, result ){
-					if( error ){
-						callback( error );
-						return;
-					}
-					//Scenarios are group of function.
-					/*
-						The problem here is that if two or more
-							scenario satisfy the behavior the flow can
-							be non probablistic.
-					*/
-					if( scenarios instanceof Array ){
-						if( typeof scenarios.every != "function" ){
-							//I don't know if this binds well.
-							scenarios.every = every;
-						}
-						scenarios.every( function( scene ){
-							scene( result, sceneCallback );
-						} );
-					}else{
-						callback( result );
-					}
-				} );
-
-		/*
-			The select function act as a normal switch statement but with super powers.
-
-			I employed several techniques to simulate flow similar to automata schematic.
-			I named this selective scenario because scenarios changes the flow of
-				the selection by either redirecting the result to another case
-				or adding more case for futher redirections.
-
-			Every scene is a condition based from the result of a case.
-			The scene can either push the flow to another case to further the data to
-				its desired value. Thus this is an enhancement of the switch statement.
-			Furthermore, I employed regex capability to the cases making each case
-				flexible.
-		*/
-		var sceneCallback = config.sceneCallback
-			|| ( config.sceneCallback = 
-				function sceneCallback( error, result, subCases ){
-					if( error ){
-						callback( error );
-						return;
-					}
-
-					/*
-						A scene can return subcases or suggested cases.
-						Sub cases are list of cases for the flow to be redirected.
-						At most possible, the case can be redirected to only one case
-							or none at all.
-						But it is also possible that the case can be redirected to any
-							two or more case.
-
-						In the case of suggested cases,
-							the case is redirected to a new case.
-					*/
-					if( subCases instanceof Array ){
-						if( typeof subCases.every != "function" ){
-							//I don't know if this binds well.
-							subCases.every = every;
-						}
-						subCases.every( function( value ){
-							config.data = value;
-							config.counter++;
-							select( )( config );
-						} );
-					}else if( subCases && typeof subCases == "object" ){
-						for( var key in subCases ){
-							cases[ key ] = subCases[ key ];
-							config.counter++;
-							config.data = key;
-							config.cases = cases;
-							select( )( config );
-						}						
-					}else{
-						callback( result );
-					}
-				} );
-
-		if( data in cases ){
-			cases[ data ]( data, caseCallback );
-		}else{
-			var caseList = Object.keys( cases );
-			caseList.push( null );
-			if( typeof caseList.every != "function" ){
-				//I don't know if this binds well.
-				caseList.every = every;
-			}
-			var hasCase = false;
-			caseList.every( function( value ){
-				if( value != null && !hasCase ){
-					if( ( new RegExp( value ) ).test( data ) ){
-						hasCase = true;
-						cases[ value ]( data, caseCallback );
-					}
-				}else if( value == null && !hasCase ){
-					callback( );
+					The condition should return true or false.
+				},
+				"scenario": function scenario( scope, redirect ){
+					Scenario modifies the scope or inspect
+						the scope.
+					It will execute certain task and redirects the flow.
 				}
-			} );
+			}
+		]
+	*/
+	var scope = {
+		"value": value
+	};
+	var processes = [ ];
+	var killProcesses = function killProcesses( callback ){
+		for( var index = 0; index < processes.length; index++ ){
+			clearTimeout( processes[ index ] );
 		}
-	} );
-}
+		callback(  );
+	}	
+	var redirect = function redirect( error, state ){
+		var namespace = this.namespace;
+		if( error ){
+			//Do this last!
+			killProcesses( function( ){
+				callback( error, scope );
+			} );
+			return false;
+		}
+
+		if( state === false ){
+			killProcesses( function( ){
+				callback( error, scope );
+			} );
+			return false;
+		}
+
+		if( typeof state == "string" ){
+
+		}else if( state instanceof Array ){
+
+		}else{
+
+		}
+	};
+
+	var processQueue = { };
+
+	for( var in dex = 0; index < cases.length; index++ ){
+		processes.push( setTimeout( function process( index ){
+			var thisCase = cases[ index ];
+			if( !( "namespace" in thisCase ) ){
+				thisCase.namespace = generateRandomMeanTime( );
+			}
+			var namespace = thisCase.namespace;
+			var condition = thisCase.condition;
+			var scenario = thisCase.scenario;
+			if( condition( scope, 
+				function subRedirect( error, state ){
+					redirect.call( { "namespace": namespace }, error, state );
+				} ) )
+			{
+				scenario( scope,
+					function subRedirect( error, state ){
+						redirect.call( { "namespace": namespace }, error, state );
+					} );
+			}
+		}, 0, index ) );
+	}
+};
